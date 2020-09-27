3903fd02a264a4cfd969cae1cc3e4da4fdd7a0cb
Naomi Ware <nware1066@yahoo.com>
2020-09-27 13:26:41 -0600

    Add instructions for helloName

--- js/8/helloName.js
+++ js/8/helloName.js
@@ -1,3 +1,5 @@
+// the challenge was to take in a name with mixed upper/lower case letters and return a name with only the first letter capitalized, or return hello world if give an empty string or no argument
+
 function hello(name) {
   console.log('test', name)
   if (name) {
